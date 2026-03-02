a22fac245e73a6010271616db36fab4079e55fe6
naelisa <naelisaraz@email.com>
2026-03-02 14:42:00 +0700

    Tambah identitas di read.php

--- api/read.php
+++ api/read.php
@@ -19,5 +19,11 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $data[] = $row;
 }
 
-echo json_encode($data, JSON_PRETTY_PRINT);
+http_response_code(200);
+
+echo json_encode([
+    "developer" => "Serli Nadia A.",
+    "npm" => "247006111080",
+    "data" => $data
+], JSON_PRETTY_PRINT);
 ?>
